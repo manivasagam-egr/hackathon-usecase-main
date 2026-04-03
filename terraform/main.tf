7d8217da6d75d1c49a7b22f11f36e826186c8470
manivasagam-egr <manivasagamegr@gmail.com>
2026-04-04 02:44:26 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,6 +27,23 @@ resource "google_container_cluster" "gke" {
 
   node_locations = ["asia-south1-a"]   # 🔥 VERY IMPORTANT (single zone)
 }
+resource "google_container_node_pool" "nodes" {
+  name       = "node-pool"
+  cluster    = google_container_cluster.gke.name
+  location   = var.region
+
+  node_config {
+    machine_type = "e2-micro"
+    disk_size_gb = 10
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+
+  node_count = 1
+}
+
 resource "google_artifact_registry_repository" "repo" {
   location      = var.region
   repository_id = "my-repo-1"
