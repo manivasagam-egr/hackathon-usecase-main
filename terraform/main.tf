4feada84d072764d11e52d932c8993caed81ce33
manivasagam-egr <manivasagamegr@gmail.com>
2026-04-04 17:15:16 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,11 +3,17 @@ provider "google" {
   region  = var.region
 }
 
+# -----------------------------
+# VPC
+# -----------------------------
 resource "google_compute_network" "vpc" {
   name                    = "devops-vpc"
   auto_create_subnetworks = false
 }
 
+# -----------------------------
+# Subnet
+# -----------------------------
 resource "google_compute_subnetwork" "subnet" {
   name          = "devops-subnet"
   ip_cidr_range = "10.0.0.0/16"
@@ -15,6 +21,9 @@ resource "google_compute_subnetwork" "subnet" {
   network       = google_compute_network.vpc.id
 }
 
+# -----------------------------
+# GKE Cluster
+# -----------------------------
 resource "google_container_cluster" "gke" {
   name     = "devops-cluster"
   location = var.region
@@ -25,21 +34,24 @@ resource "google_container_cluster" "gke" {
   remove_default_node_pool = true
   initial_node_count       = 1
 
-  node_locations = ["asia-south1-a"]   # 🔥 VERY IMPORTANT (single zone)
+  node_locations = ["us-central1-a"]   # ✅ stable zone
 }
 
+# -----------------------------
+# Node Pool
+# -----------------------------
 resource "google_container_node_pool" "nodes" {
-  name       = "node-pool"
-  cluster    = google_container_cluster.gke.name
-  location   = var.region
+  name     = "node-pool"
+  cluster  = google_container_cluster.gke.name
+  location = var.region
 
-  depends_on = [google_container_cluster.gke]   # ✅ ADD THIS
+  depends_on = [google_container_cluster.gke]   # ✅ important
 
   node_config {
     machine_type = "e2-micro"
     disk_size_gb = 10
 
-    preemptible = true   # ✅ ADD THIS (IMPORTANT)
+    preemptible = true   # ✅ avoids quota issue
 
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform"
@@ -48,6 +60,10 @@ resource "google_container_node_pool" "nodes" {
 
   node_count = 1
 }
+
+# -----------------------------
+# Artifact Registry
+# -----------------------------
 resource "google_artifact_registry_repository" "repo" {
   location      = var.region
   repository_id = "my-repo-1"
